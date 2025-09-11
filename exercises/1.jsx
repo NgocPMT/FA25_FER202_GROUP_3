8b351a578a6dadc5503b0a302e92036292ec5239
elvisDaWeird1 <anhlv.ce190674@gmail.com>
2025-09-11 13:21:15 +0700

    update 1.jsx

--- exercises/1.jsx
+++ exercises/1.jsx
@@ -1,5 +1,12 @@
+// Question 1
 let square = (num) => num*num;
+console.log(square(5));
 
-let bigger = (a,b) => a > b ? a : b;
+// Question 2
+let max = (a,b) => a > b ? a : b;
+console.log(max(5,3));
 
+// Question 3
 let squareArr = (arr) => arr.map(x => x*x);
+let a = [1, 2, 3];
+console.log(squareArr(a));

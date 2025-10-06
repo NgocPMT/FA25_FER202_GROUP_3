ec5ac552da8965968feba65367cfd38620185c99
elvisDaWeird1 <anhlv.ce190674@gmail.com>
2025-10-06 23:41:07 +0700

    rewrite in tailwind

--- src/routes/UsernameInputPage.jsx
+++ src/routes/UsernameInputPage.jsx
@@ -1,54 +1,34 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
 import { BsBook } from "react-icons/bs";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Style css
-const inputStyle = {
-  border: "none",
-  borderBottom: "1px solid #d3d3d3",
-  borderRadius: 0,
-  boxShadow: "none",
-};
-
-const formWidthStyle = {
-  width: "35%",
-};
-
-const buttonWidthStyle = {
-  width: "150px",
-};
-
-const labelTextStyle = {
-  color: "gray",
-  fontSize: "14px"
-};
 
 const UsernameInputPage = () => {
   return (
-    <Container className="d-flex flex-column align-items-center min-vh-100">
-      <h1 className="mt-3 mb-5">Easium</h1>
-      <BsBook size={100} className="mt-3 mb-3" />
-      <h2 className="my-3 font-lora">Welcome to Easium!</h2> 
-      <p className="my-3">We need a little more information to finish creating your account.</p>
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="mt-3 mb-5 text-4xl font-bold">Easium</h1>
+      <BsBook size={100} className="mt-5 mb-5" />
+      <h2 className="my-3 mt-5 text-4xl font-serif">Welcome to Easium!</h2>
+      <p className="my-3 mt-5 text-center text-lg">
+        We need a little more information to finish creating your account.
+      </p>
 
-      <p style={labelTextStyle} className="mt-3">Your full name</p>
-      <Form style={formWidthStyle} className="mb-3"> 
-        <Form.Control
+      {/* Input label */}
+      <p className="mt-5 text-gray-500 text-sm">Your full name</p>
+      <form className="w-1/3 mb-3">
+        <input
           type="text"
-          style={inputStyle}
+          className="w-full border-b border-gray-300 focus:outline-none focus:border-gray-500 py-2"
         />
-      </Form>
-    
-      <p className="my-3">Your email is</p>
-      <Button 
-        variant="dark" 
-        className="mt-3 py-2 rounded-pill"
-        style={buttonWidthStyle} 
+      </form>
+
+      <p className="my-3 text-gray-500">Your email is <span></span> </p>
+
+      <button
+        className="mt-3 py-2 w-[150px] rounded-full bg-black text-white hover:bg-gray-800 transition cursor-pointer"
       >
         Create account
-      </Button>
-    </Container>
+      </button>
+
+    </div>
   );
 };
 

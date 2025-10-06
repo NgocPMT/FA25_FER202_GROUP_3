f41fd21611332893d7cd1c4bcf1d6889629e4beb
elvisDaWeird1 <anhlv.ce190674@gmail.com>
2025-10-06 10:20:25 +0700

    const UsernameInputPage

--- src/routes/UsernameInputPage.jsx
+++ src/routes/UsernameInputPage.jsx
@@ -1,21 +1,55 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import { BsBook } from "react-icons/bs";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Style css
+const inputStyle = {
+  border: "none",
+  borderBottom: "1px solid #d3d3d3",
+  borderRadius: 0,
+  boxShadow: "none",
+};
+
+const formWidthStyle = {
+  width: "35%",
+};
+
+const buttonWidthStyle = {
+  width: "150px",
+};
+
+const labelTextStyle = {
+  color: "gray",
+  fontSize: "14px"
+};
 
 const UsernameInputPage = () => {
   return (
-        <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-            <h1>Easium</h1>
-              <BsBook/>
-            <h2>Welcome to Easium!</h2>
-            <p>We need a little more information to finish creating your account.</p>
-            <form>
-                <p>Your full name</p>
-                <input type="text" placeholder="Enter your full name" /> 
-            </form>
-            <p>Your email is</p>
-        </Container>
-    );
+    <Container className="d-flex flex-column align-items-center min-vh-100">
+      <h1 className="mt-3 mb-5">Easium</h1>
+      <BsBook size={100} className="mt-3 mb-3" />
+      <h2 className="my-3 font-lora">Welcome to Easium!</h2> 
+      <p className="my-3">We need a little more information to finish creating your account.</p>
+
+      <p style={labelTextStyle} className="mt-3">Your full name</p>
+      <Form style={formWidthStyle} className="mb-3"> 
+        <Form.Control
+          type="text"
+          style={inputStyle}
+        />
+      </Form>
+    
+      <p className="my-3">Your email is</p>
+      <Button 
+        variant="dark" 
+        className="mt-3 py-2 rounded-pill"
+        style={buttonWidthStyle} 
+      >
+        Create account
+      </Button>
+    </Container>
+  );
 };
 
 export default UsernameInputPage;

356064e094b1086602de3d8a296acfc5991ec3b5
elvisDaWeird1 <anhlv.ce190674@gmail.com>
2025-10-06 10:14:41 +0700

    UsernameInputPage

--- src/routes/UsernameInputPage.jsx
+++ src/routes/UsernameInputPage.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BsBook } from "react-icons/bs";
+
 const UsernameInputPage = () => {
-  return <div></div>;
+  return (
+        <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
+            <h1>Easium</h1>
+              <BsBook/>
+            <h2>Welcome to Easium!</h2>
+            <p>We need a little more information to finish creating your account.</p>
+            <form>
+                <p>Your full name</p>
+                <input type="text" placeholder="Enter your full name" /> 
+            </form>
+            <p>Your email is</p>
+        </Container>
+    );
 };
 
 export default UsernameInputPage;

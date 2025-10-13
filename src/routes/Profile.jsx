cf7e9af26d5d189441f0d133e04ca72a6b4ddd1f
NgocPMT <np1552005@gmail.com>
2025-10-13 21:37:08 +0700

    add routing for many routes

---
+++ src/routes/Profile.jsx
@@ -0,0 +1,7 @@
+//Viet Anh
+//Note: Profile bo phan About, nen 2 cai tab Home va About cung ko can code
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
---
+++ src/routes/Stat.jsx
@@ -0,0 +1,13 @@
+//TLoi
+import { Outlet } from "react-router";
+
+const Stat = () => {
+  return (
+    <div>
+      <h1>Stats</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Stat;

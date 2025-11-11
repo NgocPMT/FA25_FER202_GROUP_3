655665c07278ceb55e7a81544e9e6413964a6498
NgocPMT <np1552005@gmail.com>
2025-11-11 10:45:05 +0700

    implement edit post and view post features

--- src/routes/Edit.jsx
+++ src/routes/Edit.jsx
@@ -1,21 +1,56 @@
 import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import useLogOut from "@/hooks/useLogOut";
 
 import { IoWarning } from "react-icons/io5";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import Modal from "@/components/Modal";
 import { useLoader } from "@/context/LoaderContext";
 
-const Write = () => {
+const Edit = () => {
   const [isAvatarDropdownShow, setIsAvatarDropdownShow] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
   const [isModalShow, setIsModalShow] = useState(false);
   const [title, setTitle] = useState("");
+  const [post, setPost] = useState(null);
   const logOut = useLogOut();
   const editorRef = useRef(null);
   const navigate = useNavigate();
   const { showLoader, hideLoader } = useLoader();
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchPost = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/posts/${slug}`,
+          {
+            signal,
+          }
+        );
+        const data = await res.json();
+
+        if (data.user.username !== localStorage.getItem("username")) {
+          navigate("/home");
+        }
+
+        setPost(data);
+        setTitle(data.title);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.error("Fetch error:", err);
+        }
+      }
+    };
+
+    fetchPost();
+
+    return () => controller.abort();
+  }, [slug]);
 
   const toggleAvatarDropdownShow = () => {
     setIsAvatarDropdownShow(!isAvatarDropdownShow);
@@ -33,7 +68,7 @@ const Write = () => {
     setTitle(e.target.value);
   };
 
-  const handlePublish = async (isEmpty) => {
+  const handleSave = async (isEmpty) => {
     try {
       const token = localStorage.getItem("token");
 
@@ -64,7 +99,7 @@ const Write = () => {
       const content = editorRef.current?.getContent();
       const coverImageUrl = extractFirstImageURL(content);
 
-      if (!title || title.trim().length === 0) {
+      if (!title) {
         setModalMessage("Title is missing");
         openModal();
         return;
@@ -82,19 +117,22 @@ const Write = () => {
         return;
       }
 
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/posts`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          title,
-          content: JSON.stringify(content),
-          coverImageUrl,
-          status: "published",
-        }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/posts/${post.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            title,
+            content: JSON.stringify(content),
+            coverImageUrl,
+            status: "published",
+          }),
+        }
+      );
 
       if (!response.ok) {
         const err = await response.json();
@@ -129,22 +167,25 @@ const Write = () => {
 
   return (
     <>
-      <div>
-        <SimpleEditor
-          ref={editorRef}
-          title={title}
-          onTitleChange={handleTittleChange}
-          handlePublish={handlePublish}
-          isAvatarDropdownShow={isAvatarDropdownShow}
-          toggleAvatarDropdownShow={toggleAvatarDropdownShow}
-          logOut={logOut}
-        />
-      </div>
+      {post && (
+        <div>
+          <SimpleEditor
+            ref={editorRef}
+            title={title}
+            onTitleChange={handleTittleChange}
+            handlePublish={handleSave}
+            isAvatarDropdownShow={isAvatarDropdownShow}
+            toggleAvatarDropdownShow={toggleAvatarDropdownShow}
+            logOut={logOut}
+            content={post.content}
+          />
+        </div>
+      )}
       <Modal open={isModalShow} onClose={closeModal}>
         <div className="p-8 min-h-24 flex flex-col items-center">
           <IoWarning className="text-red-800 size-12 mb-4" />
           <h5 className="text-red-800 text-center text-xl mb-4 font-semibold">
-            Post Publishing Failed
+            Post Updating Failed
           </h5>
           <p role="modal-message" className="text-gray-600 text-sm">
             {modalMessage}
@@ -155,4 +196,4 @@ const Write = () => {
   );
 };
 
-export default Write;
+export default Edit;
